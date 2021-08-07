660d8e46d5206f414a728d6a569bdec3b328fb88
Mehmet Aziz Algüllü <3363217433MAziz28666774646>
2021-08-07 16:54:51 +0300

    changes Data Types of ExpMonth and ExpYear because angular receive them by other type. (old one => byte, updated => string)

--- Entities/Concrete/CreditCard.cs
+++ Entities/Concrete/CreditCard.cs
@@ -11,8 +11,8 @@ namespace Entities.Concrete
         public int CustomerId { get; set; }
         public string NameSurname { get; set; }
         public string CardNumber { get; set; }
-        public byte ExpMonth { get; set; }
-        public byte ExpYear { get; set; }
+        public string ExpMonth { get; set; }
+        public string ExpYear { get; set; }
         public string Cvc { get; set; }
         public string CardType { get; set; }
     }
